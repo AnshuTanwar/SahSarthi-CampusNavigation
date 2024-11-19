f855054ec99962c81646e63c18eb60f120a2a12f
Aanshu Tanwar <aanshutanwar07@gmail.com>
2024-11-19 12:26:04 +0530

    Add files via upload

---
+++ SahSarthi/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+
+const port = 8080;
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');  
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.ejs');  
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact.ejs');
+});
+
+app.get('/services', (req, res) => {
+    res.render('services.ejs'); 
+});
+
+app.listen(port, () => {
+    console.log(`listining to the port ${port}`);
+});
