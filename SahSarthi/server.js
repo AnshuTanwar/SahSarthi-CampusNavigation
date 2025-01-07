61e4696591b9ebcd58a2566892959a1ef8b7bf68
Aanshu Tanwar <aanshutanwar07@gmail.com>
2025-01-07 21:01:47 +0530

    Update server.js

--- SahSarthi/server.js
+++ SahSarthi/server.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about.ejs');  
-});
+});    
 
 app.get('/contact', (req, res) => {
     res.render('contact.ejs');
